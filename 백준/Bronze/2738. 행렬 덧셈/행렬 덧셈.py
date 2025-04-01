ae0c5793c167ba599c0ed8436448d9db520c1f3c
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2025-04-01 14:26:43 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,19 +1,9 @@
-n, m = map(int, input().split())
+n,m = map(int,input().split())
+arr1 = [list(map(int,input().split())) for _ in range(n)]
+arr2 = [list(map(int,input().split())) for _ in range(n)]
 
-# 행렬 A 입력 받기
-A = [list(map(int, input().split())) for _ in range(n)]
-
-# 행렬 B 입력 받기
-B = [list(map(int, input().split())) for _ in range(n)]
-
-# 결과 행렬 생성
-result = [[0] * m for _ in range(n)]
-
-# 행렬 덧셈 수행
 for i in range(n):
     for j in range(m):
-        result[i][j] = A[i][j] + B[i][j]
+        print(arr1[i][j] + arr2[i][j] , end = ' ')
 
-# 결과 출력
-for row in result:
-    print(*row)
+    print()
