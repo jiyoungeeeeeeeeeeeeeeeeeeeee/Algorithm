63da7710215a53a8ff0c18c7f153f4ce36985b65
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2025-04-02 14:15:49 +0900

    [Bronze I] Title: 세로읽기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.py
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -2,9 +2,9 @@
 n = 5
 arr = [list(map(str,input())) for _ in range(n)]
 
-max_col = max(len(row) for row in arr) # 가장 긴 열의 길이 구하기
+max_row = max(len(row) for row in arr)
 
-for j in range(15): #열
+for j in range(max_row): #열
     for i in range(n): #행
         if j < len(arr[i]):
             print(arr[i][j], end = '')
