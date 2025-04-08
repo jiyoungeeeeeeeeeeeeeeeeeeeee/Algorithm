3f3bb0a79c3437f5190027c55a30a963273bba0c
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2025-04-08 12:54:53 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -0,0 +1,12 @@
+s = input()
+
+result = [-1 for _ in range(26)]
+
+
+for i in range(len(s)):
+    index = ord(s[i]) - ord('a')
+    if result[index] == -1:
+        result[index] = i
+    
+for r in result:
+    print(r , end = ' ')
