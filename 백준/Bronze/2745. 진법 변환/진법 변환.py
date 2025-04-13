518d873fc6000cba7c7e840b582ed4ef0000b8e5
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2025-04-13 22:34:26 +0900

    [Bronze II] Title: 진법 변환, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -2,7 +2,6 @@ n,b = input().split()
 b = int(b)
 total = 0
 
-n = n[::-1]
 
 for i in range(len(n)):
     if n[i].isdigit():
@@ -10,8 +9,6 @@ for i in range(len(n)):
     else:
         value = ord(n[i].upper())-ord('A') + 10
     
-    value = value * (b**i)
-    # print(value)
-    total += value
+    total += value*(b**(len(n) -1 - i))
 
 print(total)
