0bafc63269956343f579a73d30dd9e5763a4a4d3
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2025-04-16 00:03:18 +0900

    [Bronze II] Title: 벌집, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -1,11 +1,9 @@
-# 1 -> 1개 / 2 -> 6개 (2-7) / 3 -> 12개 (8-19) / 4 -> 18개(20-37)
-
 n = int(input())
 
 floor = 1
 honeycomb_cnt = 1
 
-while n > honeycomb_cnt:
+while n >honeycomb_cnt:
 
     honeycomb_cnt += floor*6
     floor += 1 
