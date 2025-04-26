664cf2a8e167993c54a5a296897dfd878efac6fd
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2025-04-26 23:26:28 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.py
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.py
@@ -1,23 +1,21 @@
 tri = []
 
 while True:
-    a,b,c = (map(int,input().split()))
-    if a+b+c == 0 :
-        break
+    x,y,z = map(int,input().split())
 
-    tri.append([a,b,c])
+    if x+y+x == 0:
+        break    
+    tri.append((x,y,z))
 
-for a,b,c in tri:
-    max_side = max(a,b,c)
-    total = a+b+c
-
+for x,y,z in tri:
+    max_side = max(x,y,z)
+    total = x+y+z
+    
     if max_side >= total - max_side:
         print('Invalid')
-    
+    elif x == y == z:
+        print('Equilateral')
+    elif len(set((x,y,z))) == 2:
+        print('Isosceles')
     else:
-        if a==b==c:
-            print('Equilateral')
-        elif len(set([a,b,c])) == 2:
-            print('Isosceles')
-        else:
-            print('Scalene')
+        print('Scalene')
