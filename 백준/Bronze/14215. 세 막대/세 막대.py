28e926aa8fa4f02792fd06625b6df81d6cd0e4b0
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2025-04-27 01:01:43 +0900

    [Bronze III] Title: 세 막대, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/14215. 세 막대/세 막대.py
+++ 백준/Bronze/14215. 세 막대/세 막대.py
@@ -1,12 +1,9 @@
-tri = []
-a,b,c = map(int,input().split())
+tri = sorted(map(int,input().split()))
 
-tri.extend([a,b,c])
-
-max_side = max(tri)
-total = a+b+c
+max_side = tri[2] 
+total = sum(tri)
 
 if max_side < total - max_side:
     print(total)
 else:
-    print((total - max_side) + (total - max_side - 1))
+    print((total - max_side)*2- 1)
