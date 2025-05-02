302c294d4cbd8fae82fa0177dee7cda41ed91625
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2025-05-03 00:18:04 +0900

    [Bronze I] Title: 평균, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,11 +1,10 @@
 n = int(input())
 scores = list(map(int,input().split()))
-re_score = 0
-max_scores = max(scores) 
 
-for s in scores:
-    re_score += s/max_scores*100
-    
+m = max(scores)
 
-print(re_score/n)
-    
+new_scores = [score/m*100 for score in scores]
+
+average = sum(new_scores)/n
+
+print(average)
