30e2cf77e70f9b21a8b9fdbdfd40095c9ba6e25a
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2025-05-09 01:51:57 +0900

    [Silver IV] Title: 스택, Time: 400 ms, Memory: 112168 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,36 @@
+n = int(input())
+
+stack = []
+commend = ['push' , 'pop' , 'size' ,'empty','top']
+
+for i in range(n):
+    com = input()
+
+    if 'push' in com:
+        value = int(com.split()[1])
+        stack.append(value)
+    
+    if 'pop' in com:
+        if stack:
+            print(stack[-1])
+            stack.pop()
+        elif not stack:
+            print(-1)
+    
+    if 'size' in com:
+        print(len(stack))
+    
+    if 'empty' in com:
+        if not stack:
+            print(1)
+        if stack:
+            print(0)
+
+    if 'top' in com:
+        if stack:
+            print(stack[-1])
+        if not stack:
+            print(-1)
+
+
+    
