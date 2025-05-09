9ac699dde754644041a2694d9fc4be220cd4f541
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2025-05-09 23:45:00 +0900

    [Bronze III] Title: 최댓값, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.py
+++ 백준/Bronze/2566. 최댓값/최댓값.py
@@ -1,13 +1,12 @@
-arr1 = [list(map(int,input().split())) for _ in range(9)]
-max1 = arr1[0][0]
-max_row, max_col = 0,0
-
+numbers = [list(map(int,input().split())) for _ in range(9)]
+max_pos = numbers[0][0]
+m_num = -1
 for i in range(9):
     for j in range(9):
-        if arr1[i][j] > max1:
-            max1 = arr1[i][j]
-            max_row , max_col = i,j
-print(max1)
-print(f"{max_row + 1} {max_col + 1}")
-   
-      
+        if m_num < numbers[i][j]:
+            m_num = numbers[i][j]
+            max_pos = (i,j)
+        
+
+print(m_num)
+print(max_pos[0] + 1, max_pos[1] + 1)
