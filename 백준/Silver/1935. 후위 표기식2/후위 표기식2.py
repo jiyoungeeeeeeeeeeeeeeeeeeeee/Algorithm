f1a035afd6dc377f9e7f415bafce4adf832effd4
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2025-05-22 00:48:53 +0900

    [Silver III] Title: 후위 표기식2, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1935. 후위 표기식2/후위 표기식2.py
+++ 백준/Silver/1935. 후위 표기식2/후위 표기식2.py
@@ -1,29 +1,23 @@
-n = int(input()) # 1부터 26까지 
-RPN = input()    # 후위 표기식
-values = {}
+n = int(input())
+RPN = input()
+nums = [int(input()) for _ in range(n)]
 stack = []
 
-# 후위 표기식의 문자열 -> 숫자로 변환
-for i in range(n):
-    values[chr(ord('A') + i)] = int(input()) 
-
-# 연산자 / 피연산자 나눠서 스택에 저장
 for token in RPN:
-    if token.isalpha():
-        stack.append(values[token])
-    else:
-        b = stack.pop() # i-2
-        a = stack.pop() # i-1
+    if token.isalpha(): # 피연산자 일 경우
+        index = ord(token)- ord("A")
+        stack.append(nums[index]) 
+    else: # 연산자일 경우
+        b = stack.pop()
+        a = stack.pop()
 
-        # 실제 계산은 i-2 -> i-1
         if token == "+":
-            stack.append(a + b)
+            stack.append(a+b)
+        elif token == "*":
+            stack.append(a*b)
+        elif token == "/":
+            stack.append(a/b)
         elif token == '-':
-            stack.append(a - b)
-        elif token == '*':
-            stack.append(a * b)
-        elif token == '/':
-            stack.append(a / b)
-
+            stack.append(a-b)
 
 print(f"{stack[0]:.2f}")
