93ccaaae935e3abfcd8cd63f727321f477851007
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2025-05-28 02:04:29 +0900

    [Silver IV] Title: 접미사 배열, Time: 80 ms, Memory: 37196 KB -BaekjoonHub

---
+++ 백준/Silver/11656. 접미사 배열/접미사 배열.py
@@ -0,0 +1,16 @@
+import sys
+from collections import deque
+
+S = sys.stdin.readline().rstrip()
+s_dq = deque(S)
+stack = [s_dq.copy()] # 처음 상태 포함
+
+for s in range (len(S)-1):
+    s_dq.popleft()
+    stack.append(s_dq.copy())
+
+result = [''.join(d) for d in stack]
+result.sort()
+
+for r in result:
+    print(r)
