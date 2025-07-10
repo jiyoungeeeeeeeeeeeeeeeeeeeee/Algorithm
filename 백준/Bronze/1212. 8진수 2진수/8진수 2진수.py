5e9bd89df3d43f10b6f381c71432f2e3e542ecc3
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2025-07-11 03:10:12 +0900

    [Bronze II] Title: 8진수 2진수, Time: 136 ms, Memory: 52960 KB -BaekjoonHub

--- 백준/Bronze/1212. 8진수 2진수/8진수 2진수.py
+++ 백준/Bronze/1212. 8진수 2진수/8진수 2진수.py
@@ -1,11 +1,5 @@
 import sys
 
 octal = sys.stdin.readline().strip()
-
-for i in range(len(octal)):
-    o = octal[i]
-    b = bin(int(o))[2:]
-    if i == 0:
-        print(b, end='')
-    else:
-        print(b.zfill(3), end='')
+binary = ''.join(bin(int(d))[2:].zfill(3) for d in octal)
+print(binary.lstrip('0') or '0')
