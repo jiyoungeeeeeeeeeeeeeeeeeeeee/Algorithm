e4ae54a1d29d4f684839c462ab064bcb42aa7c81
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2025-07-12 02:02:32 +0900

    [Silver II] Title: 골드바흐 파티션, Time: 3364 ms, Memory: 42352 KB -BaekjoonHub

--- 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
+++ 백준/Silver/17103. 골드바흐 파티션/골드바흐 파티션.py
@@ -1,23 +1,25 @@
-import sys
-input = sys.stdin.readline
+import sys , math
+T = int(sys.stdin.readline())
 
-T = int(input())
-Ns = [int(input()) for _ in range(T)]
-maxN = max(Ns)
-
-# 1) 최대 N까지 한 번만 에라토스테네스의 체로 소수 여부 계산
-is_prime = [True] * (maxN + 1)
+MAX = 1_000_000
+is_prime = [True] * (MAX+1)
 is_prime[0] = is_prime[1] = False
-for i in range(2, int(maxN**0.5) + 1):
+
+
+for i in range(2, int(math.sqrt(MAX))+ 1):
     if is_prime[i]:
-        for j in range(i*i, maxN + 1, i):
+        for j in range(i*i , MAX+1 , i):
             is_prime[j] = False
 
-# 2) 각 테스트 케이스마다 골드바흐 파티션 개수 세기
-for N in Ns:
+
+n_list = [int(input()) for _ in range(T)]
+output = []
+
+for n in n_list:
     cnt = 0
-    # p ≤ N//2 까지만 검사하면 (p, N-p) 쌍의 중복을 막을 수 있어
-    for p in range(2, N//2 + 1):
-        if is_prime[p] and is_prime[N - p]:
+    for p in range(2, n // 2 + 1):
+        if is_prime[p] and is_prime[n-p]:
             cnt += 1
-    print(cnt)
+    output.append(str(cnt))
+
+print('\n'.join(output))
