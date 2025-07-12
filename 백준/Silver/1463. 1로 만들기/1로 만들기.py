195a38afcf4c593126f2e0a7f7c6c2f0822bb6a2
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2025-07-13 07:50:30 +0900

    [Silver III] Title: 1로 만들기, Time: 116 ms, Memory: 118552 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,13 +1,15 @@
 import sys
+X = int(sys.stdin.readline())
 
-x = int(sys.stdin.readline())
-dp = [0]*(x+1)
+dp = [0] * (X+1)
 
-for i in range(2 , x+1):
+for i in range(2, X+1):
     dp[i] = dp[i-1] + 1
+
     if i%2 == 0:
         dp[i] = min(dp[i] , dp[i//2] + 1)
+        
     if i%3 == 0:
         dp[i] = min(dp[i] , dp[i//3] + 1)
 
-print(dp[x])
+print(dp[X])
