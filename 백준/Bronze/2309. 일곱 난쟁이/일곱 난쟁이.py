60628452ec1d86cdfbdcc4082c1b0b09477677f1
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2025-08-15 03:15:07 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -4,7 +4,7 @@ total = sum(dwarf)
 fake_dwarf = total - 100
 
 for i in range(9):
-    for j in range(i+1 , 9):
+    for j in range(i):
         if dwarf[i] + dwarf[j] == fake_dwarf:
             fake1 = dwarf[i]
             fake2 = dwarf[j]
@@ -14,6 +14,7 @@ for i in range(9):
     else:
         continue
     break
+    
 dwarf.sort()
 for f in dwarf:
     print(f)
