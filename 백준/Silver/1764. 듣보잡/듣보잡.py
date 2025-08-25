94a3dee1a3ccf64d7c2bbe27bca2135c63003a00
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2025-08-26 01:54:17 +0900

    [Silver IV] Title: 듣보잡, Time: 76 ms, Memory: 44192 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -2,17 +2,9 @@ import sys
 input = sys.stdin.readline
 n,m = map(int,input().split())
 
-listen = set()
-see = set()
-
-for _ in range(n):
-    l = input().strip()
-    listen.add(l)
-
-for _ in range(m):
-    s = input().strip()
-    see.add(s)
+listen = {input().strip() for _ in range(n)}
+see = {input().strip() for _ in range(m)}
 
 same = sorted(listen & see)
 print(len(same))
-print(*same , sep= '\n')
+print(*same , sep = '\n')
