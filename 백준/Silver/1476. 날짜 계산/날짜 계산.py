55e90314da397221887de4b8dceb93f94818486d
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2025-08-27 01:46:06 +0900

    [Silver V] Title: 날짜 계산, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1476. 날짜 계산/날짜 계산.py
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.py
@@ -1,9 +1,9 @@
 e,s,m = map(int,input().split())
-year = 1
+year = e
 
 while True:
-    if (year - 1) % 15 + 1 == e and (year - 1) % 28 + 1 == s and (year - 1) % 19 + 1 == m:
+    if (year - 1) % 28 + 1 == s and (year - 1) % 19 + 1 == m:
         print(year)
         break
     else:
-        year += 1
+        year += 15
