9f8db8005966981c9505f02c34c43d00fd5bdba7
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2025-09-04 05:23:10 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.py
@@ -0,0 +1,15 @@
+import sys
+n = int(sys.stdin.readline())
+
+ans = 0
+start = 1
+digits = 1
+
+
+while start * 10 <= n:
+    ans += (start * 10 - start) * digits
+    start *= 10
+    digits += 1
+
+ans += (n - start + 1 ) * digits 
+print(ans)
