5b252aa2ad9ac604270bab7092cda77faee51021
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2025-11-11 02:20:57 +0900

    [Silver IV] Title: 붙임성 좋은 총총이, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/26069. 붙임성 좋은 총총이/붙임성 좋은 총총이.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(input())
+meet = {'ChongChong'}
+
+for _ in range(n):
+    a,b = sys.stdin.readline().split()
+    
+    if a in meet:
+        meet.add(b)
+    elif b in meet:
+        meet.add(a)
+
+print(len(meet))
