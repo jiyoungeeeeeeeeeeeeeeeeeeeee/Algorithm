850e0c1ff3b6d54fbeee616bdb9f4a4f2cbf9948
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2025-12-01 06:01:09 +0900

    [Bronze II] Title: 하얀 칸, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1100. 하얀 칸/하얀 칸.py
+++ 백준/Bronze/1100. 하얀 칸/하얀 칸.py
@@ -1,20 +1,18 @@
 import sys
-cnt1 = 0
-cnt2 = 0
+cnt = 0
 
 for i in range(8):
     chess = sys.stdin.readline().strip()
-    if i == 0 or i%2 == 0:
-        white1 = chess[::2]  
-        for w1 in white1:
-            if w1 == "F":
-                cnt1 += 1
+
+    if i%2 == 0:
+        white = chess[::2]  
 
     else:
-        white2 = chess[1::2]
-        for w2 in white2:
-            if w2 == "F":
-                cnt2 += 1
+        white = chess[1::2]
+    
+    for ch in white:
+        if ch == "F":
+            cnt += 1
 
-print(cnt1+cnt2)
+print(cnt)
     
