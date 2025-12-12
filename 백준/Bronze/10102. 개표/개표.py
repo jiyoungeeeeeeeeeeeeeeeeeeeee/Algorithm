dd84e8f84a8700771c69b4e121eb6a843ab287cc
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2025-12-13 04:21:02 +0900

    [Bronze III] Title: 개표, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10102. 개표/개표.py
@@ -0,0 +1,14 @@
+import sys
+
+v = int(sys.stdin.readline())
+vote = sys.stdin.readline().strip()
+
+a = vote.count('A')
+b = vote.count('B')
+
+if a>b:
+    print('A')
+elif a<b:
+    print('B')
+else:
+    print('Tie')
