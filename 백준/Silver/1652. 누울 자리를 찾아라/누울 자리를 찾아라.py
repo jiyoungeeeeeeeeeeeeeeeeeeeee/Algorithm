2e85901d31fa5cc03eb53bb3bbae3c7dfb319c7a
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2026-01-17 05:29:07 +0900

    [Silver V] Title: 누울 자리를 찾아라, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
+++ 백준/Silver/1652. 누울 자리를 찾아라/누울 자리를 찾아라.py
@@ -5,31 +5,25 @@ row = 0
 col = 0
 total = []
 
+def Room(rooms): 
+    cnt = 0
+    for r in rooms:
+        if len(r) >= 2:
+            cnt += 1
+    return cnt
+
 for _ in range(n):
     room = sys.stdin.readline().rstrip()
     lr = list(room)
     total.append(lr)
-
-    rroom = room.split('X')
-
-    for r in rroom:
-        if r == '':
-            pass
-        else:
-            if len(r) >= 2:
-                row += 1
+    row += Room(room.split('X'))
+    
 
 for i in range(n):
     strr = ''
     for j in range(n):
         strr += total[j][i]
+    col += Room(strr.split('X'))
 
-    croom = strr.split('X')
-
-    for c in croom:
-        if c == '':
-            pass
-        else:
-            if len(c) >= 2:
-                col += 1
 print(row ,col)
+
