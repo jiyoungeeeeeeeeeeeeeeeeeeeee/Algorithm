9f5606440cf83721a1b7ee865530f73495968c63
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2026-02-20 06:03:45 +0900

    [Silver V] Title: 반지, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/5555. 반지/반지.py
+++ 백준/Silver/5555. 반지/반지.py
@@ -6,14 +6,7 @@ cnt = 0
 
 for i in range(n):
     ring = sys.stdin.readline().strip()
-    h = 0
-
-    while h < len(ring):
-        if find in ring:
-            cnt += 1
-            break
-        else:
-            ring = ring[1:] + ring[:1]
-            h += 1
-
+    if find in (ring + ring):
+        cnt += 1
+    
 print(cnt)
