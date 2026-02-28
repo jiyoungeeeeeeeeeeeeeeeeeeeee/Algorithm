1ee4b88924912a6ce2887c20d11f2de70244a018
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2026-03-01 06:12:44 +0900

    [Silver IV] Title: 숫자놀이, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1755. 숫자놀이/숫자놀이.py
+++ 백준/Silver/1755. 숫자놀이/숫자놀이.py
@@ -5,31 +5,24 @@ m,n = map(int,sys.stdin.readline().split())
 nums = [i for i in range(m,n+1)]
 
 dic = {0:'zero',1:'one', 2:'two', 3:'three', 4:'four', 5:'five',6:'six',7:'seven',8:'eight',     9:'nine'}
-trans = []
 
-for n in nums:
-    if n >= 10:
-        a,b = n//10 , n%10
-        comb = dic.get(a)+ ' ' + dic.get(b)
-        trans.append(comb)
+pair = []
+
+for x in nums:
+    if x >= 10:
+        a,b = x//10 , x%10
+        word = dic.get(a)+ ' ' + dic.get(b)
     else:
-        trans.append(dic.get(n))
-trans.sort()
+        word = dic.get(x)
+    pair.append((word,x))
+pair.sort()
 
-rev = {}
-for k,v in dic.items():
-    rev[v] = k
 
-result = []
-for t in trans:
-    if len(t.split()) == 1:
-        result.append(rev.get(t))
+cnt = 0
+for word, x in pair:
+    cnt += 1
+    if cnt % 10 == 0:
+        print(x)
     else:
-        a,b = t.split()
-        A,B = rev.get(a) , rev.get(b)
-        i = str(A)+str(B)
-        result.append(i)
-
-for j in range(((n-m)//10)+1):
-    print(*result[j*10:10*(j+1)])
-    
+        print(x, end=" ")
+        
