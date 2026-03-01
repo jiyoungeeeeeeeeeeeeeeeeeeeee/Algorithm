3a271dc8091ab73a2b3df17dd891b8b088145767
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2026-03-02 05:57:58 +0900

    [Silver V] Title: 복붙의 달인, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11008. 복붙의 달인/복붙의 달인.py
+++ 백준/Silver/11008. 복붙의 달인/복붙의 달인.py
@@ -4,16 +4,6 @@ t = int(sys.stdin.readline())
 
 for _ in range(t): 
     s,p = sys.stdin.readline().split()    
-    i = 0
-    cnt = 0
 
-    while i < len(s):
-
-        if s[i:len(p)+i] == p:
-            cnt += 1
-            i += len(p)
-        else:
-
-            i += 1
-
-    print(cnt + (len(s) - cnt * len(p)))
+    cnt = s.count(p)
+    print(len(s) - cnt * len(p) + cnt)
