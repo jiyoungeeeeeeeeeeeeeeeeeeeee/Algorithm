2b8beac44fb49c94826e5f8964b1b9dfc0b5f75c
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2026-03-24 04:34:08 +0900

    [Silver V] Title: 학생 인기도 측정, Time: 60 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/25325. 학생 인기도 측정/학생 인기도 측정.py
+++ 백준/Silver/25325. 학생 인기도 측정/학생 인기도 측정.py
@@ -10,14 +10,14 @@ for _ in range(n):
     love = sys.stdin.readline().split()
     c.update(love)
 
-blank = set(c)
-orgin = set(names)
-missing = orgin-blank
+cname = set(c)
+origin = set(names)
+missing = origin-cname
 
 for m in missing:
     result.append((m , 0))
 
-for k,v in sorted(c.items()):
+for k,v in c.items():
     result.append((k,v))
 
 result.sort(key = lambda x : (-x[1] , x[0]))
