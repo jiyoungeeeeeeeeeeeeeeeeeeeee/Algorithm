5d6678dfbbe3bbe432da68b57977fa5b04138a4b
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2026-03-24 04:36:18 +0900

    [Silver V] Title: 학생 인기도 측정, Time: 56 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/25325. 학생 인기도 측정/학생 인기도 측정.py
+++ 백준/Silver/25325. 학생 인기도 측정/학생 인기도 측정.py
@@ -2,26 +2,19 @@ import sys
 from collections import Counter
 
 n = int(sys.stdin.readline())
-names = list(sys.stdin.readline().split())
+names = sys.stdin.readline().split()
 c = Counter()
-result = []
 
 for _ in range(n):
     love = sys.stdin.readline().split()
     c.update(love)
 
-cname = set(c)
-origin = set(names)
-missing = origin-cname
+result = []
 
-for m in missing:
-    result.append((m , 0))
+for name in names:
+    result.append((name, c[name]))
 
-for k,v in c.items():
-    result.append((k,v))
+result.sort(key=lambda x: (-x[1], x[0]))
 
-result.sort(key = lambda x : (-x[1] , x[0]))
-
-for r in result:
-    print(*r)
-    
+for name, count in result:
+    print(name, count)
