b2350e5d9e359fe6b37f6152c08c805ae5798eed
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2026-04-16 04:48:51 +0900

    [Silver IV] Title: 수 찾기, Time: 428 ms, Memory: 48604 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,12 +1,21 @@
 import sys
 
-n = int(input())
-arr1 = set(map(int,sys.stdin.readline().split()))
-m = int(input())
-arr2 = list(map(int,sys.stdin.readline().split()))
+n = int(sys.stdin.readline())
+arr1 = sorted(list(map(int, sys.stdin.readline().split())))
+m = int(sys.stdin.readline())
+arr2 = list(map(int, sys.stdin.readline().split()))
+
+def binary_search(arr, target):
+    l, r = 0, len(arr)-1
+    while l <= r:
+        mid = (l + r) // 2
+        if arr[mid] == target:
+            return True
+        elif arr[mid] < target:
+            l = mid + 1
+        else:
+            r = mid - 1
+    return False
 
 for x in arr2:
-    if x in arr1:
-        print(1)
-    else:
-        print(0)
+    print(1 if binary_search(arr1, x) else 0)
