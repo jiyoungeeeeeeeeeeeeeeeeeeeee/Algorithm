74bafdc1e9b8ae9d172760597ff90f12c642c7f5
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2026-04-24 04:33:00 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 44 ms, Memory: 32412 KB, Score: 50 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -8,14 +8,14 @@ for i in range(len(s)):
     for c in range(26):
         lst[c][i+1] = lst[c][i]
     
-    
     idx = ord(s[i]) - ord ('a')
     lst[idx][i+1] += 1
   
+
 for _ in range(q):
     a,l,r = sys.stdin.readline().split()
     l,r = int(l),int(r)
     find = ord(a) - ord('a')
     result = lst[find][r+1] - lst[find][l]
-    print(result)
-    
+
+    sys.stdout.write(str(result) + '\n')    
