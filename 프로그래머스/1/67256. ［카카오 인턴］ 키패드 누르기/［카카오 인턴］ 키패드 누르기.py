d4c8f7d550ba1d150601e926c6b241902a845cef
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2026-05-11 04:57:32 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.19 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.py
@@ -1,50 +1,44 @@
 def solution(numbers, hand):
     answer = ''
-
+    lp = (0,3) 
+    rp = (2,3)
     pos = {
         1: (0, 0), 2: (1, 0), 3: (2, 0),
         4: (0, 1), 5: (1, 1), 6: (2, 1),
         7: (0, 2), 8: (1, 2), 9: (2, 2),
         0: (1, 3)
     }
-
-    left_pos = (0, 3)
-    right_pos = (2, 3)
-
     if hand == "right":
-        main_hand = 'R'
-    else:
-        main_hand = 'L'
-
+        hand = 'R'
+    elif hand == 'left':
+        hand = 'L'
+    
     for n in numbers:
         target = pos[n]
-
-        if n in [1, 4, 7]:
+        
+        if n in [1,4,7]:
             answer += 'L'
-            left_pos = target
-
-        elif n in [3, 6, 9]:
+            lp = target
+                
+        elif n in [3,6,9]:
             answer += 'R'
-            right_pos = target
-
+            rp = target
+        
         else:
-            left_distance = abs(left_pos[0] - target[0]) + abs(left_pos[1] - target[1])
-            right_distance = abs(right_pos[0] - target[0]) + abs(right_pos[1] - target[1])
-
-            if left_distance < right_distance:
+            ld = abs(lp[0] - target[0]) + abs(lp[1] - target[1])
+            rd = abs(rp[0] - target[0]) + abs(rp[1] - target[1])
+            
+            if rd > ld:
                 answer += 'L'
-                left_pos = target
-
-            elif left_distance > right_distance:
+                lp = target
+            elif rd < ld:
                 answer += 'R'
-                right_pos = target
-
+                rp = target
             else:
-                answer += main_hand
-
-                if main_hand == 'L':
-                    left_pos = target
+                answer += hand
+                
+                if hand == 'L':
+                    lp = target
                 else:
-                    right_pos = target
-
+                    rp = target
     return answer
