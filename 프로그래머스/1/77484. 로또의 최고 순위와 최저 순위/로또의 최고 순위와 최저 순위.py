5931756170266d809271dabbcaa19b471eee9ab9
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2026-05-26 04:54:46 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.06 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -1,5 +1,4 @@
 def solution(lottos, win_nums):
-    answer = []
     dic = {6:1,5:2,4:3,3:4,2:5,1:6,0:6} # 맞춘 갯수 : 순위
     z = lottos.count(0)
     
@@ -8,11 +7,5 @@ def solution(lottos, win_nums):
     MAX = cnt + z
     MIN = cnt 
     
-    a = dic.get(MAX)
-    b = dic.get(MIN)
-    
-    answer.append(a)
-    answer.append(b)
-    
-    return answer
+    return [dic[MAX] , dic[MIN]]
     
