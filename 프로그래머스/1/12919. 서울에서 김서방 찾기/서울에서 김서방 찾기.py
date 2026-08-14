d3b0ece0c987d4509df2c0e48a2ea7be7bad2dca
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2026-08-15 04:24:29 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.01 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,8 +1,4 @@
 def solution(seoul):
-    answer = '김서방은 '
-    answer2 = '에 있다'
-    dic = {}
-    for i in range(len(seoul)):
-        dic[seoul[i]] = i
-        
-    return answer + str(dic['Kim']) + answer2
+    idx = seoul.index('Kim')
+    
+    return f"김서방은 {idx}에 있다"
