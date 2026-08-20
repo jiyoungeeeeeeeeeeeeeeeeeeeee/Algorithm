c1d9a1b709df59eff5ee9338ea2e6070a092ff2c
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2026-08-21 05:05:22 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,16 +1,11 @@
 def solution(num):
     cnt = 0
-    if num == 1:
-        return 0
     
-    while cnt < 500 :
+    while num != 1 and cnt < 500 :
         if num % 2 == 0 :
             num //= 2
         else:
             num = num*3 + 1
         cnt += 1
         
-        if num == 1:
-            return cnt
-    
-    return -1
+    return cnt if num == 1 else -1
