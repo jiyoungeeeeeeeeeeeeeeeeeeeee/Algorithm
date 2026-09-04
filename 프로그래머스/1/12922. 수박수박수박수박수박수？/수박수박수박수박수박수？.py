02f44c291a9a8dc293bea921f66508242333c8d3
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2026-09-05 05:50:39 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.02 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.py
@@ -1,20 +1,3 @@
 def solution(n):
-    answer = ''
-    a = '수'
-    b = '박'
-    cnt = 0
-    
-    if n == 1:
-        return a
-    elif n == 2 :
-        return a+b
-    
-    else:
-        while cnt < n:
-            answer += a
-            cnt += 1
-            
-            if cnt < n:
-                answer += b
-                cnt += 1
-        return answer
+
+    return ('수박'*n)[:n]
