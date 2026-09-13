7bf57d3106dcdcaa5ac00025480a213b9f4493d7
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2026-09-14 05:38:52 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.py
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -1,14 +1,11 @@
+from itertools import combinations
+
 def solution(number):
     answer = 0
-    number.sort()
     
-    for i in range(len(number)):
-        for j in range(i + 1, len(number)):
-            for k in range(j + 1, len(number)):
-                
-                if number[i] + number[j]+ number[k] == 0:
-                    answer += 1
-                    
-                
+    for c in combinations(number,3):
+        if sum(c) == 0:
+            answer += 1
+    
                 
     return answer
